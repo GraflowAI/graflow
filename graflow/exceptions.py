636282a203fc50ec9451dd349e85110240a231b8
Makoto Yui <myui@apache.org>
2025-07-16 03:33:04 +0900

    Add custom exceptions module for Graflow library

---
+++ graflow/exceptions.py
@@ -0,0 +1,9 @@
+"""
+graflow.exceptions
+=================
+This module defines custom exceptions for the Graflow library.
+"""
+
+class GraflowError(Exception):
+    """Base exception class for Graflow errors."""
+    pass
