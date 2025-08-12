7640ed9cf1bec38caa55beb62b8bd3045fc55b62
Makoto Yui <myui@apache.org>
2025-08-12 14:16:33 +0900

    refactor: Remove unnecessary parent directory path insertion in run_example.py

--- examples/run_example.py
+++ examples/run_example.py
@@ -4,9 +4,6 @@
 import os
 import sys
 
-# Add the parent directory to Python path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python run_example.py <example_name>")
